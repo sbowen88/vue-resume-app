744c2e8836a8b56140dd6321cc3a1650ba2ed567
sbowen88 <sbowey@gmail.com>
2019-02-28 10:57:52 -0800

    app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<div id="app">
+  <router-view></router-view>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+    name: 'app'
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* Add new fonts here */
+@import '../node_modules/roboto-fontface/css/roboto/roboto-fontface.css';
+@import '../node_modules/font-awesome/css/font-awesome.css';
+@import '../node_modules/material-design-icons/iconfont/material-icons.css';
+@import '../node_modules/source-sans-pro/source-sans-pro.css';
+@import '../node_modules/npm-font-open-sans/open-sans.css';
+@import '../node_modules/npm-font-open-sans-condensed/open-sans-condensed.css';
+@import '../node_modules/raleway-webfont/raleway.css';
+body {
+  padding: 0;
+  margin: 0;
+  overflow-x: hidden;
+  background: #CCCCCC;
 }
 </style>
